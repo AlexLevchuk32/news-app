389e0fbf0f5953db7a0054c36737227ddf655d3c
Alex Levchuk <alexlevchuk@mail.ru>
2024-02-01 19:53:32 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// Кастомный HTTP запрос
+function customHttp() {
+	return {
+		get(url, cb) {
+			try {
+				const xhr = new XMLHttpRequest();
+				xhr.open('GET', url);
+				xhr.addEventListener('load', () => {
+					if (Math.floor(xhr.status / 100) !== 2) {
+						cb(`Error. Status code: ${xhr.status}`, xhr);
+						return;
+					}
+					const response = JSON.parse(xhr.responseText);
+					cb(null, response);
+				});
+
+				xhr.addEventListener('error', () => {
+					cb(`Error. Status code: ${xhr.status}`, xhr);
+				});
+
+				xhr.send();
+			} catch (error) {
+				cb(error);
+			}
+		},
+		post(url, body, headers, cb) {
+			try {
+				const xhr = new XMLHttpRequest();
+				xhr.open('POST', url);
+				xhr.addEventListener('load', () => {
+					if (Math.floor(xhr.status / 100) !== 2) {
+						cb(`Error. Status code: ${xhr.status}`, xhr);
+						return;
+					}
+					const response = JSON.parse(xhr.responseText);
+					cb(null, response);
+				});
+
+				xhr.addEventListener('error', () => {
+					cb(`Error. Status code: ${xhr.status}`, xhr);
+				});
+
+				if (headers) {
+					Object.entries(headers).forEach(([key, value]) => {
+						xhr.setRequestHeader(key, value);
+					});
+				}
+
+				xhr.send(JSON.stringify(body));
+			} catch (error) {
+				cb(error);
+			}
+		},
+	};
+}
+// Инициализация http запроса
+const http = customHttp();
+
+//  Инициализация селекторов
+document.addEventListener('DOMContentLoaded', function () {
+	// Инициализая библиотеки Materialize
+	M.AutoInit();
+});
