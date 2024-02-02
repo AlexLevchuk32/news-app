a530e063b3b1a7178156f705c29aea856f0a408f
Alex Levchuk <alexlevchuk@mail.ru>
2024-02-03 01:05:19 +0300

    add news card request and render

--- app.js
+++ app.js
@@ -56,8 +56,75 @@ function customHttp() {
 // Инициализация http запроса
 const http = customHttp();
 
+const newsService = (function () {
+	const apiKey = '569868670af740deb2712cbd01190c5c';
+	const apiUrl = 'https://newsapi.org/v2';
+
+	return {
+		topHeadLines(country = 'ru', getNews) {
+			http.get(
+				`${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`,
+				getNews,
+			);
+		},
+		everything(query, getNews) {
+			http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, getNews);
+		},
+	};
+})();
+
 //  Инициализация селекторов
 document.addEventListener('DOMContentLoaded', function () {
 	// Инициализая библиотеки Materialize
 	M.AutoInit();
+	loadNews();
 });
+
+// Загрузка новостей
+function loadNews() {
+	newsService.topHeadLines('us', getResponse);
+}
+
+// Функция calback, будет вызываться после получения ответа от сервера (новостей)
+function getResponse(error, response) {
+	// console.log(response); // articles[Array] массив объектов с новостями
+
+	renderNews(response.articles);
+}
+
+// Рендер новостей
+function renderNews(news) {
+	const newsContainer = document.querySelector('.news-container .row');
+	let fragment = '';
+
+	news.forEach((newsItem) => {
+		const elem = newsTemplate(newsItem);
+
+		fragment += elem;
+	});
+
+	// console.log(fragment);
+
+	newsContainer.insertAdjacentHTML('afterbegin', fragment);
+}
+
+// Разметка для новостей
+function newsTemplate({ urlToImage, title, url, description }) {
+	// console.log(news);
+	return `
+		<div class='col s12'>
+			<div class='card'>
+				<div class='card-image'>
+					<img src='${urlToImage || null}'>
+					<span class='card-title'>${title || ''}</span>
+				</div>
+				<div class='card-content'>
+					<p>${description || ''}</p>
+				</div>
+				<div class='card-action'>
+					<a href='${url || null}' target="_blank">Read more...</a>
+				</div>
+			</div>
+		</div>
+	`;
+}
